b8cee272bdb4c1c5e14163c5799ab691ba421496
ficazam <felipeicaza@gmail.com>
2025-12-22 14:30:30 -0500

    adds health component

--- apps/gb-api/src/app.module.ts
+++ apps/gb-api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardGateway } from './board/board.gateway';
 import { BoardService } from './board/board.service';
+import { HealthController } from './health.controller';
 
 @Module({
   providers: [BoardGateway, BoardService],
+  controllers: [HealthController],
 })
 export class AppModule {}
